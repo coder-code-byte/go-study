9ee8585e0b173eb81197e769b80b692b7427571b
smiths <may950617@gmail.com>
2019-08-10 19:42:56 +0800

    添加代码

---
+++ middleware/jwt/jwt.go
@@ -0,0 +1,39 @@
+package jwt
+
+import (
+	"net/http"
+	"time"
+
+	"gin.com/pkg/error"
+	"gin.com/pkg/util"
+	"github.com/gin-gonic/gin"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var code int
+		var data interface{}
+		code = error.SUCCESS
+		token := c.Query("token")
+		if token == "" {
+			code = error.INVALID_PARAMS
+		} else {
+			claims, err := util.ParseToken(token)
+			if err != nil {
+				code = error.ERROR_AUTH_CHECK_TOKEN_FAIL
+			} else if time.Now().Unix() > claims.ExpiresAt {
+				code = error.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+			}
+		}
+		if code != error.SUCCESS {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": code,
+				"msg":  error.GetMsg(code),
+				"data": data,
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
